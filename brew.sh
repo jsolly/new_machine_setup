301e95ef9a21c23e56c46aeacee816860a806358
john8739 <jsolly@esri.com>
2020-05-27 09:43:42 -0700

    initial commit

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+brew install grep # How do I automatically set PATH to include GNU grep?
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install brave-browser
+brew cask install spotify
+brew cask install discord
+brew cask install skype
+brew cask install pycharm-ce
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
+
